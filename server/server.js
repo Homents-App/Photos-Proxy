468a282edf972b7df08b24ef8eda216adfabbb47
Daniel Lee <dsl8va@gmail.com>
2020-12-15 15:22:51 -0800

    update for redis get

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api', (req, res, next) => {
 	let url = req.url;
 	let split = url.split('/');
 	let id = split[split.length-1]
-	console.log('middleware', id, typeof(id));
+
 	client.exists(id, (err, reply) => {
 		if (reply === 1) {
 			console.log('Pulling from Redis cache')

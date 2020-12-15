8610712dda0062159dc6e74289c038505c3a1806
Daniel Lee <dsl8va@gmail.com>
2020-12-14 17:02:09 -0800

    working on redis middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const router = require('./router');
+const client = require('./router/')
 
 const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
 const app = express();
@@ -13,7 +14,11 @@ app.get('/loaderio-da775ca393b463698d924dd5f047a5aa.txt', (req, res) => {
 	res.sendFile(path.join(__dirname, '../loaderio.txt'));
 })
 
-
+// Middleware to catch any IDs that are in the Redis cache
+app.use((req, res, next) => {
+	console.log('req middleware: ', req);
+	next();
+})
 
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
 app.use('/bundles', router.bundles);
